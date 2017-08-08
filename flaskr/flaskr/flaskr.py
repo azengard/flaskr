fb4068a5bfae63dfb738e23b475f2a3f51384675
Carlos Henrique <azengard@users.noreply.github.com>
2017-08-07 22:59:53 -0300

    Add routes

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -53,3 +53,43 @@ def initdb_command():
     print('Initialized the database.')
 
 
+@app.route('/')
+def show_entries():
+    db = get_db()
+    cur = db.execute('select title, desc from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)',
+                [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfuly posted')
+    return redirect(url_for('show_entries'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.methods == POST:
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid Username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid Password'
+        else:
+            session['logged_in'] = True
+            flash('You are logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You are logged out')
+    return redirect(url_for('show_entries'))
